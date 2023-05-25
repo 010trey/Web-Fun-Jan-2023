6eb8679763757abed0a69ac54f92b0b549ccf61e
MAC <mac@MACs-MacBook-Air.local>
2023-05-25 23:31:42 +0100

    user.py

---
+++ Week2/Day 1/Practice/algos.py
@@ -0,0 +1,23 @@
+
+
+
+
+const newi9nv1 = [
+    {name: "grain of rice", quantity: 9000},
+    {name: "peanut butter", quantity: 50},
+    {name: "royal jelly", quantity: 20},
+]
+
+const newinv1 = [
+    {name: "grain of rice", quantity: 1},
+    {name: "peanut butter", quantity: 20}
+]
+
+
+
+const expected = [
+    {name: "grain of rice", quantity: 70},
+    {name: "peanut butter", quantity: 9001},
+    {name: "royal jelly", quantity: 20},
+]
+const newinv
--- python/user.py
+++ python/user.py
@@ -1,32 +1,35 @@
-#slippin' PMm — Yesterday at 2:31 PM
+# #slippin' PMm — Yesterday at 2:31 PM
 
-class User:
-    def init(self,name,lastname,email,age):
-        self.first_name =name
-        self.last_name =lastname
-        self.email=email
-        self.age = age
-        self.rewards_member = False
-        self.gold_card_points = 0
-    def display_info(self):
-        print("first name",self.first_name)
-        print("last name",self.last_name)
-        print(" email",self.email)
-        print("age",self.age)
-        print("rewards member",self.rewards_member)
-        print("gold card points",self.gold_card_points)
-    def enroll(self):
-        self.rewards_member=True
-        self.gold_card_points=200
-    def spend_points(self, amount) :
-        self.gold_card_points= self.gold_card_points - amount
+# class User:
+#     def __init__(self):
+#         self.first_name = "mary"
+#         self.last_name = "pewee"
+#         self.email= "marypewwee@gmail.com"
+#         self.age = 25
+#         self.rewards_member = False
+#         self.gold_card_points = 0
+        
+        
+        
+#     def display_info(self):
+#         print(self.first_name)
+#     # self_pewee = self()
+#         print(self.last_name)
+#     # self_email = self()
+#         print(self.email)
+#     # self_age = self()
+#         print(self.age)
+        
+# user=User()
+# user.display_info()
 
-amine=User("amine","hahaha","test@gmail.com",20)
-aziz=User("aziz","hhihihi","aaaa@gmm.tn",66)
-amine.display_info()
-amine.enroll()
-amine.display_info()
-amine.spend_points(50)
-amine.display_info()
-aziz.enroll()
-aziz.spend_points(80)
+# a = False
+# b = 0
+
+# if b > a:
+#     print("rewards_member = False")
+# else:
+#     print("gold_card_points = 0")
+# user=User()
+
+# user.display_info()
