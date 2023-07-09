590d1e118d0bdf9e67d01968619926828abccec6
MAC <mac@MACs-MacBook-Air.local>
2023-07-09 22:49:32 +0100

    yellow belt

---
+++ yellow belt /script.js
@@ -0,0 +1,12 @@
+function chooseLunch(element) {
+    alert("contact us at 555-5555");
+}
+function BUY(){
+    document.querySelector("#cont-p").innerHTML++;
+}
+// function BUY() {    
+//         for(var num = 0; num <= 5; num++){
+//             console.log(num);
+//         }
+//     }
+    
