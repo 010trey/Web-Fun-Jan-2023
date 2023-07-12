59ef99afb55e15f75e28ac6f58f5fd52d49fdef7
MAC <mac@MACs-MacBook-Air.local>
2023-07-13 00:49:01 +0100

    python

--- python/w01-D02/user/BankAccount.py
+++ python/w01-D02/user/BankAccount.py
@@ -1,19 +1,15 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
     def __init__(self, balance, int_rate=0.335): 
         self.int_rate = int_rate
         self.balance=balance
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
+        
     def deposit(self, amount):
         self.balance += amount
-        # your code here
     def withdraw(self, amount):
         if self.balance > amount:
             self.balance-=amount+5
         else:
             print("Insufficient funds")
-        # your code here
     def display_account_info(self):
         print(f"Balance: {self.balance} - Interest_rate: {self.int_rate}")
     def yield_interest(self):
